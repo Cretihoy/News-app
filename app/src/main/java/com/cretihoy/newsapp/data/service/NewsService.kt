78f02f8c117d00fcbe42be6ab4acf6a5a9ccc79f
cretihoy <sergzhio238@gmail.com>
2024-01-30 18:32:41 +0300

    fix service

--- app/src/main/java/com/cretihoy/newsapp/data/service/NewsService.kt
+++ app/src/main/java/com/cretihoy/newsapp/data/service/NewsService.kt
@@ -1,11 +1,19 @@
 package com.cretihoy.newsapp.data.service
 
+import com.cretihoy.newsapp.data.model.Category
+import com.cretihoy.newsapp.data.model.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val API_KEY = "6ce37518fa0a4776af79bf8ee21c120d"
 
 interface NewsService {
 
-    @GET("/v2/top-headlines/business/$API_KEY")
-    suspend fun getAllBusinessNews(): Any
+    @GET("/v2/top-headlines")
+    suspend fun getAllBusinessNews(
+        @Query("category") category: Category,
+        @Query("apiKey") apiKey: String = API_KEY,
+        @Query("pageSize") pageSize: Int = 20,
+        @Query("page") page: Int = 1
+    ): List<Response>
 }

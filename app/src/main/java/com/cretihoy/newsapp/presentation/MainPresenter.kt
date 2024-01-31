7706b24e8455433a670f1d88d4154be9e8c14a71
cretihoy <sergzhio238@gmail.com>
2024-01-31 13:05:17 +0300

    add logic to view articles in presenter and view

--- app/src/main/java/com/cretihoy/newsapp/presentation/MainPresenter.kt
+++ app/src/main/java/com/cretihoy/newsapp/presentation/MainPresenter.kt
@@ -1,10 +1,28 @@
 package com.cretihoy.newsapp.presentation
 
+import com.cretihoy.newsapp.data.mapper.ArticleMapper
+import com.cretihoy.newsapp.data.newmodel.Category
+import com.cretihoy.newsapp.data.service.NewsService
+import kotlinx.coroutines.launch
 import moxy.InjectViewState
 import moxy.MvpPresenter
+import moxy.presenterScope
 import javax.inject.Inject
 
 @InjectViewState
 class MainPresenter
-@Inject constructor() : MvpPresenter<MainView>() {
+@Inject constructor(
+    private val service: NewsService,
+    private val mapper: ArticleMapper
+) : MvpPresenter<MainView>() {
+
+    fun loadNews() {
+        presenterScope.launch {
+            val models = Category.values().map { category ->
+                val response = service.getAllNewsByCategory(category)
+                mapper.mapArticle(response, category)
+            }
+            viewState.populate(models)
+        }
+    }
 }
--- app/src/main/java/com/cretihoy/newsapp/presentation/MainView.kt
+++ app/src/main/java/com/cretihoy/newsapp/presentation/MainView.kt
@@ -1,6 +1,12 @@
 package com.cretihoy.newsapp.presentation
 
+import com.cretihoy.newsapp.domain.model.CategoryModel
 import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.StateStrategyType
 
 interface MainView : MvpView {
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun populate(model: List<CategoryModel>)
 }

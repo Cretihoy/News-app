f7dfd955b490d2b7ec0b060d0ae550d5f7411c9f
cretihoy <sergzhio238@gmail.com>
2024-01-31 13:04:43 +0300

    create article mapper

---
+++ app/src/main/java/com/cretihoy/newsapp/data/mapper/ArticleMapper.kt
@@ -0,0 +1,31 @@
+package com.cretihoy.newsapp.data.mapper
+
+import com.cretihoy.newsapp.data.newmodel.ArticleDto
+import com.cretihoy.newsapp.data.newmodel.Category
+import com.cretihoy.newsapp.data.newmodel.CategoryResponse
+import com.cretihoy.newsapp.domain.model.ArticleModel
+import com.cretihoy.newsapp.domain.model.CategoryModel
+import javax.inject.Inject
+
+class ArticleMapper
+@Inject constructor() {
+
+    fun mapArticle(
+        response: CategoryResponse,
+        category: Category
+    ): CategoryModel {
+        return CategoryModel(
+            articles = response.articles.map {
+                mapArticle(it)
+            },
+            categoryName = category.title
+        )
+    }
+
+    private fun mapArticle(article: ArticleDto): ArticleModel {
+        return ArticleModel(
+            imageUrl = article.urlToImage,
+            title = article.title
+        )
+    }
+}
